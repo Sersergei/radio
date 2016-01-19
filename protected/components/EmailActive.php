b82ec5818b2727a2f19dba03efe3c8be4f381e9b
Sergei <sersergei@bigmir.net>
2016-01-19 21:49:47 +0200

    Мейл активацыи

--- protected/components/EmailActive.php
+++ protected/components/EmailActive.php
@@ -25,7 +25,7 @@ class EmailActive
             $href = Yii::app()->getBaseUrl(true) . '/register/active/id/' . $this->user->id_user . '/linc/' . $linc;
             $text =  '<br>' . Yii::t('radio', 'Thank you for your request to be added to the web radiomusictest.com We add names to our list only after verifying the recipient`s permission, which is why we are sending you this address confirmation request To confirm that you would like to receive our future invite on the music tests, please ') . '<a href =' . $href .'>'.Yii::t('radio','click here.') . '</a>';
             $subject = Yii::t('radio','Registration');
-            $email = Yii::app()->params['adminEmail'];
+            $email = $this->user->radio->radiostationSettings->email;
             $headers = "From: radio <{$email}>\r\n" .
                 "Reply-To: {$email}\r\n" .
                 "MIME-Version: 1.0\r\n" .
