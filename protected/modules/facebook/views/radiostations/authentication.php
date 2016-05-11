e20a7d337368bb5e3e7b731c50de5f34374ce1d6
Sergei <seresrgei@bigmir.net>
2016-05-11 14:40:11 +0300

    получение данніх с фейсбука

---
+++ protected/modules/facebook/views/radiostations/authentication.php
@@ -0,0 +1,39 @@
+<header>
+    <div id="lang"><a class="by" href="?lang=ru"></a><a class="en" href="?lang=en"></a><a class="uk" href="?lang=uk"></a> <a class="et" href="?lang=et"></a></div>
+</header>
+<?php  $path=new ServiceUserIdentity();
+$path=$path->seturl(); ?>
+<div id="facebook">
+<a href="<?php echo $path ?>"><?php echo Yii::t('radio','Login with Facebook'); ?></a>
+    <br>
+    <?php echo Yii::t('radio','Or'); ?>
+</div>
+<div class="registration">
+
+    <?php $form=$this->beginWidget('CActiveForm', array(
+        'id'=>'users-form',
+        // Please note: When you enable ajax validation, make sure the corresponding
+        // controller action is handling ajax validation correctly.
+        // There is a call to performAjaxValidation() commented in generated controller code.
+        // See class documentation of CActiveForm for details on this.
+        'enableAjaxValidation'=>false,
+        'clientOptions'=>array(
+            'validateOnChange'=>true,
+            'validateOnSubmit'=>true
+        ),
+    )); ?>
+
+
+
+    <div class="row">
+        <?php echo $form->labelEx($model,'email'); ?><br><br>
+        <?php echo $form->textField($model,'email',array('size'=>40,'maxlength'=>40)); ?><br><br>
+        <?php echo $form->error($model,'email'); ?>
+    </div>
+    <div class="row buttons">
+        <?php echo CHtml::submitButton('Next'); ?>
+    </div>
+
+    <?php $this->endWidget(); ?>
+
+</div><!-- form -->
---
+++ protected/modules/facebook/views/radiostations/index.php
@@ -0,0 +1,13 @@
+
+<header>
+    <div id="lang"><a class="by" href="?lang=ru"></a><a class="en" href="?lang=en"></a><a class="uk" href="?lang=uk"></a> <a class="et" href="?lang=et"></a></div>
+</header>
+
+<div class="message">
+
+
+
+
+<?php echo $model; ?><br>
+<?php echo CHtml::button(Yii::t('radio', 'Next'), array('class'=>'never','submit' => array($buton))); ?>
+</div>
